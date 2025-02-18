79f79a6efd1443217087e055f6b61c2bb28b9007
Rushikesh Kate <rushikeshkate8@gmail.com>
2025-02-18 22:13:49 +0530

    First commit

---
+++ TestingPractice/app/src/test/java/com/rushikesh/testingpractice/EmailValidatorTest.kt
@@ -0,0 +1,18 @@
+package com.rushikesh.testingpractice
+
+import com.rushikesh.testingpractice.util.EmailValidator
+import junit.framework.TestCase.assertFalse
+import junit.framework.TestCase.assertTrue
+import org.junit.Assert
+import org.junit.Test
+
+class EmailValidatorTest {
+    @Test
+    fun emailValidator_emptyString_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail(""))
+    }
+    @Test
+    fun emailValidator_validEmail_ReturnsTrue() {
+        assertTrue(EmailValidator.isValidEmail("rushikesh@gmail.com"))
+    }
+}
